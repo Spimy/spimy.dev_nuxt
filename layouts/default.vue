6ef3663ef04d81274f1f910628c658603d0bf317
Spimy <officialspyme@gmail.com>
2023-02-04 00:30:59 +0400

    Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <Head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <title>Spimy - {{ getPageName() }}</title>
+  </Head>
+
+  <header>
+    <Navbar />
+  </header>
+
+  <slot />
+
+</template>
+
+<script lang="ts" setup>
+function getPageName() {
+  const route = useRoute();
+
+  if (route.name === undefined || route.name === null || route.name === "index") {
+    return "Home"
+  }
+
+  const routeName = route.name.toString();
+  return routeName.charAt(0).toUpperCase() + routeName.slice(1);
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
