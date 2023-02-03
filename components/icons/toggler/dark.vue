a6c6ca8a2fc47754ee9e639efed313eccd2f53ee
Spimy <officialspyme@gmail.com>
2023-02-04 00:31:32 +0400

    Add dark theme toggler icon

---
+++ components/icons/toggler/dark.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <ellipse cx="12.4613" cy="11.398" rx="11.5385" ry="11.398" fill="#505050" />
+    <ellipse cx="12.4617" cy="11.3979" rx="9.23077" ry="9.11837" fill="#505050" />
+    <path
+      d="M7.99408 3.64736L17.2248 19.4408C12.8098 21.9588 7.16439 20.4645 4.61538 16.1033C2.06638 11.742 3.57899 6.16537 7.99408 3.64736Z"
+      fill="#212121" />
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+svg {
+  cursor: pointer;
+}
+</style>
