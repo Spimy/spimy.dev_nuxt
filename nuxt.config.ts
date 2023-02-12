292d2da9ab9f60ae0aee658a4c4840ca8067b98b
Spimy <officialspyme@gmail.com>
2023-02-12 17:44:15 +0400

    Add default meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
+const DOMAIN = 'https://spimy.dev';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'UTF-8',
+      viewport: 'width=device-width, initial-scale=1.0, user-scalable=no',
+      meta: [
+        { 'http-equiv': 'x-ua-compatible', content: 'IE=edge' },
+        {
+          name: 'keywords',
+          content:
+            'spimy, portfolio, william, programming, projects, code, javascript, typescript, skills, contact'
+        },
+        { property: 'og:site:name', content: "Spimy's Portfolio" },
+        { property: 'og:see_also', content: DOMAIN },
+        { property: 'og:image', content: `${DOMAIN}/logos/character.png` },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:image', content: `${DOMAIN}/logos/character.png` }
+      ],
+      link: [{ rel: 'canonical', href: DOMAIN }]
+    }
+  },
+
   typescript: { shim: false },
   modules: ['@nuxtjs/color-mode'],
   css: ['@/assets/scss/main.scss', '@/assets/scss/theme.scss'],
@@ -18,4 +41,4 @@ export default defineNuxtConfig({
     preference: 'system',
     fallback: 'light'
   }
-})
+});
