f31ce8e139f45840323da095efaef5e539912d03
Spimy <officialspyme@gmail.com>
2023-02-16 16:08:46 +0400

    Add solid icons for fontawesome

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,8 +1,9 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faTwitter, faInstagram, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faTwitter, faInstagram, faGithub, faYoutube);
+library.add(faTwitter, faInstagram, faGithub, faYoutube, faAngleLeft, faAngleRight);
 config.autoAddCss = false;
 
 export default defineNuxtPlugin((nuxtApp) => {
