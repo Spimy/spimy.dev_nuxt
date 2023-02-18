315bce8da3fdbebadb3d2122c8569b4751c6507c
Spimy <officialspyme@gmail.com>
2023-02-18 21:33:53 +0400

    Add fontawesome icons for contact form

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,9 +1,9 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faTwitter, faInstagram, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faAngleLeft, faMessage, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faTwitter, faInstagram, faGithub, faYoutube, faAngleLeft, faAngleRight);
+library.add(faTwitter, faInstagram, faGithub, faYoutube, faAngleLeft, faAngleRight, faMessage, faEnvelope, faUser);
 config.autoAddCss = false;
 
 export default defineNuxtPlugin((nuxtApp) => {
