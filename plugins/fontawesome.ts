0ff6fd266c1ea6ee3b77c426161343d3c320f195
Spimy <officialspyme@gmail.com>
2023-03-07 20:38:35 +0400

    Register required fontawesome icons

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -2,8 +2,22 @@ import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faTwitter, faInstagram, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faAngleRight, faAngleLeft, faMessage, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faCircleCheck, faCircleXmark } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faTwitter, faInstagram, faGithub, faYoutube, faAngleLeft, faAngleRight, faMessage, faEnvelope, faUser);
+library.add(
+  faTwitter,
+  faInstagram,
+  faGithub,
+  faYoutube,
+  faAngleLeft,
+  faAngleRight,
+  faMessage,
+  faEnvelope,
+  faUser,
+  faClock,
+  faCircleCheck,
+  faCircleXmark
+);
 config.autoAddCss = false;
 
 export default defineNuxtPlugin((nuxtApp) => {
