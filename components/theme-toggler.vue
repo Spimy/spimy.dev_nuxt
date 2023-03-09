73b95fcb98c2a9a7bc64b28c9a1c23ae3574acf3
Spimy <officialspyme@gmail.com>
2023-03-09 18:26:20 +0400

    Set opacity instead of display

--- components/theme-toggler.vue
+++ components/theme-toggler.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <IconsTogglerLight @click="toggleTheme" class="toggler" :class="{ show: colorMode.value === 'light' }" />
-    <IconsTogglerDark @click="toggleTheme" class="toggler" :class="{ show: colorMode.value === 'dark' }" />
+  <div id="toggler">
+    <IconsTogglerLight
+      tabindex="0"
+      @keyup.enter="toggleTheme"
+      @click="toggleTheme"
+      class="icon"
+      :class="{ show: colorMode.value === 'light' }"
+    />
+    <IconsTogglerDark
+      tabindex="0"
+      @keyup.enter="toggleTheme"
+      @click="toggleTheme"
+      class="icon"
+      :class="{ show: colorMode.value === 'dark' }"
+    />
   </div>
 </template>
 
@@ -14,16 +26,18 @@ function toggleTheme() {
 </script>
 
 <style lang="scss" scoped>
-.toggler {
-  display: none;
-}
+#toggler {
+  position: relative;
 
-.toggler.show {
-  display: block;
+  .icon {
+    inset: 50%;
+    position: absolute;
+    opacity: 0;
+    transform: translate(-50%, -50%);
 
-  -webkit-animation: fadeInFromNone 0.5s ease-in-out;
-  -moz-animation: fadeInFromNone 0.5s ease-in-out;
-  -o-animation: fadeInFromNone 0.5s ease-in-out;
-  animation: fadeInFromNone 0.5s ease-in-out;
+    &.show {
+      opacity: 1;
+    }
+  }
 }
 </style>
