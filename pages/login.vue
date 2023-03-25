48d88b17cb4173918775ba00d8ca684da149deef
Spimy <officialspyme@gmail.com>
2023-03-25 15:23:48 +0400

    Add login page

---
+++ pages/login.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="authenticator">
+    <form>
+      <h1 class="title">Login to Dashboard</h1>
+      <div class="input">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" v-model="formData.email" />
+      </div>
+      <div class="input">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" v-model="formData.password" />
+      </div>
+      <div class="controller">
+        <button type="submit" class="btn" @click.prevent="submit">Login</button>
+        <NuxtLink to="https://account.spimy.dev/forgot-password" rel="external" target="_blank"
+          >Forgot Password</NuxtLink
+        >
+      </div>
+    </form>
+
+    <VueHcaptcha
+      ref="captcha"
+      :sitekey="config.public.hcaptcha_sitekeys.login"
+      :theme="$colorMode.value"
+      size="invisible"
+      @verify="hcaptchaHandler.onVerify"
+      @expired="hcaptchaHandler.onExpire"
+      @challengeExpired="hcaptchaHandler.onExpire"
+      @error="hcaptchaHandler.onError"
+    />
+
+    <Message :show="messageConfig.show" :message="messageConfig.message" :type="messageConfig.type" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import VueHcaptcha from '@hcaptcha/vue3-hcaptcha';
+import { HCaptcha } from '@/utils/types/hcaptcha';
+
+definePageMeta({
+  layout: 'admin'
+});
+
+const formData = reactive({
+  email: '',
+  password: ''
+});
+
+const submit = () => {
+  if (captcha.value) {
+    console.log('Submitting hCaptcha and contact form...');
+    captcha.value.execute();
+  }
+};
+
+const config = useRuntimeConfig();
+
+type MessageType = 'success' | 'error' | 'inprogress';
+const messageConfig = reactive({
+  show: false,
+  message: '',
+  type: 'inprogress' as MessageType
+});
+
+// Delay in seconds
+const showMessage = (message: string, type: MessageType, delay?: number) => {
+  messageConfig.message = message;
+  messageConfig.show = true;
+  messageConfig.type = type;
+
+  if (delay) {
+    setTimeout(() => {
+      messageConfig.show = false;
+    }, delay * 1000);
+  }
+};
+
+const captcha = ref<VueHcaptcha | null>(null);
+const hCaptcha: HCaptcha = reactive({
+  verified: false,
+  expired: false,
+  token: '',
+  eKey: '',
+  error: new Error()
+});
+
+const hcaptchaHandler = new HCaptchaHandler(hCaptcha, () => {
+  useAuthFetch('/login', { method: 'POST', body: { ...formData, captcha: hCaptcha } }).then((response: any) => {
+    localStorage.setItem('accessToken', response.tokens.access);
+    localStorage.setItem('refreshToken', response.tokens.refresh);
+    showMessage(response.message, 'success', 3);
+    navigateTo('/admin');
+  });
+});
+</script>
+
+<style lang="scss" scoped>
+.authenticator {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  height: 100svh;
+  margin: -6rem 2rem;
+
+  form {
+    font-size: 1rem;
+    background-color: var(--secondary-clr);
+    padding: 2em;
+    border-radius: 1em;
+    max-width: 30em;
+    width: 100%;
+
+    .title {
+      font-size: 1.5em;
+      margin-bottom: 1em;
+      margin-top: 0;
+    }
+
+    .input {
+      display: flex;
+      gap: 0.5em;
+      margin-bottom: 2em;
+      border: 0.1em solid var(--border-clr);
+      border-radius: 1em;
+      padding: 0.5em;
+
+      label {
+        border-right: 0.1em solid;
+        border-color: inherit;
+        padding-right: 0.5em;
+      }
+
+      input {
+        font-size: 1em;
+        background-color: transparent;
+        border: none;
+        color: var(--text-clr);
+        outline: none;
+        width: 100%;
+      }
+
+      &:focus-within {
+        border-color: var(--primary-clr);
+      }
+    }
+
+    .controller {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .btn {
+        // width: 100%;
+        border-radius: 1em;
+        font-weight: bold;
+      }
+
+      a {
+        font-size: 0.9em;
+        text-decoration: none;
+
+        &:hover,
+        &:focus {
+          text-decoration: underline;
+        }
+      }
+    }
+  }
+}
+</style>
