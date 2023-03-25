1f4b79be7f42dfad006481bbfc548ca1d3f40967
Spimy <officialspyme@gmail.com>
2023-03-25 20:17:53 +0400

    Fix navigation errors

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -3,20 +3,10 @@ export default defineNuxtRouteMiddleware((to) => {
     const { isAuthenticated, user } = response;
     const isAdmin = user?.role === 'admin';
 
-    if (!isAuthenticated && to.path === '/admin') {
-      navigateTo('/login');
-      return response;
-    }
-
-    if (isAuthenticated && !isAdmin && to.path === '/admin') {
+    if ((!isAuthenticated || (isAuthenticated && !isAdmin)) && to.path === '/admin') {
       logout();
-      navigateTo('/login');
-      return;
-    }
-
-    if (isAuthenticated && to.path === '/login') {
+    } else if (isAuthenticated && to.path === '/login') {
       navigateTo('/admin');
-      return response;
     }
   });
 });
