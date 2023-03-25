9e4311ae6ef8d76fece605f1d56738135ac1f8e3
Spimy <officialspyme@gmail.com>
2023-03-25 19:37:51 +0400

    Add logout function to delete session

--- utils/auth.ts
+++ utils/auth.ts
@@ -3,7 +3,18 @@ import { AuthCheckResponse } from '@/utils/types/responses';
 export const localLogout = () => {
   localStorage.removeItem('accessToken');
   localStorage.removeItem('refreshToken');
+  localStorage.removeItem('sessionId');
   navigateTo('/login');
+};
+
+// Delete the session on the auth backen and logout locally
+export const logout = () => {
+  const sessionId = localStorage.getItem('sessionId');
+  if (!sessionId) return localLogout();
+
+  useAuthFetch(`/sessions?id=${sessionId}`, { method: 'DELETE' }).then((response) => {
+    if (response.status === 200) return localLogout();
+  });
 };
 
 // Check if user is authenticated with auth backend
