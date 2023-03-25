70e38064f14194f3fd07d7e99ec7d984d309828c
Spimy <officialspyme@gmail.com>
2023-03-26 00:27:58 +0400

    Add admin page

---
+++ pages/admin/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <h1>Admin Dashboard</h1>
+    <div v-if="userData.role === 'admin' && data">
+      <div v-for="project in data.projects" :key="project._id.toString()">
+        <NuxtLink :to="`/admin/projects/${project._id}`">
+          {{ project.title }}
+        </NuxtLink>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth']
+});
+
+const { userData } = useUser();
+
+const { data } = useFetch('/api/projects');
+</script>
+
+<style lang="scss" scoped></style>
