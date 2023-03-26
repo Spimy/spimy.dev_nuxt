87384ab6a331663ff8c87218ea59786d44ed94f8
Spimy <officialspyme@gmail.com>
2023-03-27 00:53:16 +0400

    Add basic styling to main admin page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,17 +1,18 @@
 <template>
   <main>
-    <h1>Admin Dashboard</h1>
-    <div v-if="userData.role === 'admin' && data">
-      <div v-for="project in data.projects" :key="project._id.toString()">
-        <NuxtLink :to="`/admin/projects/${project._id}`">
-          {{ project.title }}
-        </NuxtLink>
+    <h1>Admin Dashboard - Projects</h1>
+    <div v-if="userData?.role === 'admin' && !!data" class="card">
+      <div v-for="project in data.projects" :key="project._id.toString()" class="project-container">
+        <h2>{{ project.title }}</h2>
+        <NuxtLink :to="`/admin/projects/${project._id}`">Edit</NuxtLink>
       </div>
     </div>
   </main>
 </template>
 
 <script lang="ts" setup>
+import { ProjectsResponse } from '@/utils/types/responses';
+
 definePageMeta({
   layout: 'admin',
   middleware: ['auth']
@@ -19,7 +20,37 @@ definePageMeta({
 
 const { userData } = useUser();
 
-const { data } = useFetch('/api/projects');
+const { data } = useFetch<ProjectsResponse>('/api/projects');
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  margin-block: -3rem;
+
+  h1 {
+    font-size: 2.5rem;
+  }
+
+  .card {
+    font-size: 1rem;
+    padding: 0.5em 1em;
+    background-color: var(--secondary-clr);
+    border-radius: 0.5em;
+
+    .project-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.2em 1em;
+      margin: 1em 0;
+      background-color: var(--background-clr);
+      color: var(--text-clr);
+      border-radius: inherit;
+
+      h2 {
+        font-size: 1em;
+      }
+    }
+  }
+}
+</style>
