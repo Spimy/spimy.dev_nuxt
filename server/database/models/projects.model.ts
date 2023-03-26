058ca94d5d7541d57cc6cfa01e569d386e6d10ea
Spimy <officialspyme@gmail.com>
2023-03-26 14:04:42 +0400

    Extend Document in interface

--- server/database/models/projects.model.ts
+++ server/database/models/projects.model.ts
@@ -1,6 +1,6 @@
-import { Schema, model } from 'mongoose';
+import { Document, Schema, model } from 'mongoose';
 
-export interface IProjects {
+export interface IProjects extends Document {
   title: string;
   link: string;
   description: string;
