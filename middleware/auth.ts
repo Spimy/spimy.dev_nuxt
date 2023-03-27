6856f7bc2afcb91a896cf3ce60e5e3d4459d805e
Spimy <officialspyme@gmail.com>
2023-03-27 22:56:07 +0400

    Add admin check to middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,18 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path === '/login' && from.path !== 'login') return;
 
-  checkAuthentication().then((isAuthenticated) => {
+  checkAuthentication().then(({ isAuthenticated, data }) => {
     if (!isAuthenticated && to.path !== '/login') {
+      logout();
+
+      /**
+       * Ignore path from login because that route is handled by the client after logging in
+       * This is to avoid fast routing which causes error with `insertBefore`
+       *
+       * WHEN WILL THIS BE FIXED?? IT'S STILL AN OPEN ISSUE ON GITHUB
+       * GitHub issue: https://github.com/nuxt/nuxt/issues/13350
+       */
+    } else if (isAuthenticated && data?.user.role !== 'admin' && from.path !== '/login') {
       logout();
     } else if (isAuthenticated && to.path === '/login') {
       navigateTo('/admin');
