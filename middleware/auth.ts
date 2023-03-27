9c8aaf0fcbe41e8f22d1ec70b6eba7ae40b1dbd7
Spimy <officialspyme@gmail.com>
2023-03-27 23:29:15 +0400

    Remove unused argument

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,4 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware((to) => {
   checkAuthentication().then(({ isAuthenticated, data }) => {
     if (!isAuthenticated && to.path !== '/login') {
       logout();
