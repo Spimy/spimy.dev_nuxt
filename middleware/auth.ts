25630908cd8c58e9fc307d8962cf4616d1d0859e
Spimy <officialspyme@gmail.com>
2023-03-27 23:28:01 +0400

    Remove unnecessary route check

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  if (to.path === '/login' && from.path !== 'login') return;
-
   checkAuthentication().then(({ isAuthenticated, data }) => {
     if (!isAuthenticated && to.path !== '/login') {
       logout();
