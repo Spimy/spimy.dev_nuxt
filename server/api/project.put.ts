41201cbe967a0d925a3a2bb93460bd6eb6a035fc
Spimy <officialspyme@gmail.com>
2023-03-27 17:03:22 +0400

    Add authentication check before updating document

--- server/api/project.put.ts
+++ server/api/project.put.ts
@@ -1,6 +1,6 @@
+import fs from 'fs';
 import { readFiles, FieldsAndFiles } from 'h3-formidable';
 import { Projects } from '@/server/database/models/projects.model';
-import fs from 'fs';
 
 interface PersistentFile {
   filepath: string;
@@ -21,38 +21,58 @@ interface EditProjectData extends FieldsAndFiles {
 }
 
 export default defineEventHandler(async (event) => {
-  const { fields, files } = (await readFiles(event, { includeFields: true })) as EditProjectData;
-  const { title, technologies, description } = fields;
+  const config = useRuntimeConfig();
+  const authHeader = getHeader(event, 'Authorization');
 
-  let previewImageUrl: string | undefined;
-  if (files.previewImage) {
-    const config = useRuntimeConfig();
-    const file = files.previewImage[0];
-    const image = fs.readFileSync(file.filepath);
+  // Check if user is authenticated before editing
+  // This check is done server side to prevent this endpoint from being accessible by everyone
+  return await checkServerAuthentication(authHeader || '').then(async ({ isAuthenticated, data }) => {
+    if (!isAuthenticated) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: 'Unauthorized',
+        data
+      });
+    }
 
-    const formData = new FormData();
-    formData.append('file', new Blob([image], { type: file.mimetype }), file.originalFilename);
-    formData.append('folder', 'projects');
+    const { fields, files } = (await readFiles(event, { includeFields: true })) as EditProjectData;
+    const { _id, title, technologies, description } = fields;
 
-    const response = await $fetch<{ url: string }>('https://static.spimy.dev/upload', {
-      method: 'POST',
-      headers: { Authorization: config.static_secret },
-      body: formData
-    }).catch(() => undefined);
-    previewImageUrl = response?.url;
-  }
+    // If there was a file, then upload it to the static host
+    let previewImageUrl: string | undefined;
+    if (files.previewImage) {
+      const file = files.previewImage[0];
+      const image = fs.readFileSync(file.filepath);
 
-  await Projects.findByIdAndUpdate(
-    fields._id[0],
-    {
-      $set: {
-        title: title[0],
-        technologies: JSON.parse(technologies[0]),
-        description: description[0],
-        previewImageUrl: previewImageUrl
-      }
-    },
-    { new: true }
-  );
-  return {};
+      const formData = new FormData();
+      formData.append('file', new Blob([image], { type: file.mimetype }), file.originalFilename);
+      formData.append('folder', 'projects'); // Upload to https://static.spimy.dev/projects/<file_name>.<ext>
+
+      const response = await $fetch<{ url: string }>('https://static.spimy.dev/upload', {
+        method: 'POST',
+        headers: { Authorization: config.static_secret },
+        body: formData
+      }).catch(() => undefined);
+      previewImageUrl = response?.url;
+    }
+
+    // Can return null if project does not exist
+    const updatedProject = await Projects.findByIdAndUpdate(
+      _id[0],
+      {
+        $set: {
+          title: title[0],
+          technologies: JSON.parse(technologies[0]),
+          description: description[0],
+          previewImageUrl: previewImageUrl
+        }
+      },
+      { new: true }
+    );
+
+    return {
+      project: updatedProject,
+      message: updatedProject ? 'Successfully updated project.' : `Project '${_id[0]}' could not be found.`
+    };
+  });
 });
