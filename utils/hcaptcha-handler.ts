0c7948319c0e11018011fee3121fc607f34db60a
Spimy <officialspyme@gmail.com>
2023-03-28 19:26:37 +0400

    Refactor to have hcaptcha config within the util class

--- utils/hcaptcha-handler.ts
+++ utils/hcaptcha-handler.ts
@@ -1,27 +1,41 @@
 import { HCaptcha } from '@/utils/types/hcaptcha';
 
 export class HCaptchaHandler {
-  constructor(private hCaptcha: HCaptcha, private verifySuccessCallback: Function) {}
+  private hCaptchaConfig: HCaptcha;
+
+  constructor(private verifySuccessCallback: Function) {
+    this.hCaptchaConfig = reactive({
+      verified: false,
+      expired: false,
+      token: '',
+      eKey: '',
+      error: new Error()
+    });
+  }
 
   onExpire = () => {
-    this.hCaptcha.verified = false;
-    this.hCaptcha.token = '';
-    this.hCaptcha.eKey = '';
-    this.hCaptcha.expired = true;
+    this.hCaptchaConfig.verified = false;
+    this.hCaptchaConfig.token = '';
+    this.hCaptchaConfig.eKey = '';
+    this.hCaptchaConfig.expired = true;
     console.log('hCaptcha has expired...');
   };
 
   onError = (err: Error) => {
-    this.hCaptcha.token = '';
-    this.hCaptcha.eKey = '';
-    this.hCaptcha.error = err;
+    this.hCaptchaConfig.token = '';
+    this.hCaptchaConfig.eKey = '';
+    this.hCaptchaConfig.error = err;
     console.log(`Error submitting hCaptcha: ${err}`);
   };
 
   onVerify = (token: string, ekey: string) => {
-    this.hCaptcha.verified = true;
-    this.hCaptcha.token = token;
-    this.hCaptcha.eKey = ekey;
+    this.hCaptchaConfig.verified = true;
+    this.hCaptchaConfig.token = token;
+    this.hCaptchaConfig.eKey = ekey;
     this.verifySuccessCallback();
   };
+
+  get hCaptcha(): HCaptcha {
+    return this.hCaptchaConfig;
+  }
 }
